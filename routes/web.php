774805fa560c9152b4a181876309385d1a495f5a
AndreasNikitas <160608284+AndreasNikitas@users.noreply.github.com>
2025-10-06 17:06:25 +0300

    edit form

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,18 @@ Route::view('/tasks/create', 'create')
     ->name('task.create');
 
 
-Route::get('/tasks/{id}', function ($id)  {
-    return view('show', ['task' => Task::findOrFail($id)]);
+Route::get('/tasks/{id}/edit', function ($id)  {
+    return view('edit',[
+        'task' => Task::findOrFail($id)
+    ]);
+})  ->name('task.edit');
 
+
+
+Route::get('/tasks/{id}', function ($id)  {
+    return view('show',[
+        'task' => Task::findOrFail($id)
+    ]);
 })  ->name('task.show');
 
 
